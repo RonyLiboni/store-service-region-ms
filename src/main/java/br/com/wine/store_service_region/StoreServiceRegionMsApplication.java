f7a8ca2be45516ea94a2e0aaadab05e137cb6d55
RonyLiboni <ronald.liboni@acad.pucrs.br>
2022-11-07 14:00:35 -0300

    FEAT adds initial dependencies needed for the API and data needed to connect in MySQL

---
+++ src/main/java/br/com/wine/store_service_region/StoreServiceRegionMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.wine.store_service_region;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreServiceRegionMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreServiceRegionMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wine/store_service_region/StoreServiceRegionMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wine.store_service_region;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreServiceRegionMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
